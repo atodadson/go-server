39c79eb33228a6b09b300c3528cbcdaaddafeaac
atodadson <benjamindadson32@gmail.com>
2025-06-03 09:40:59 +0000

    First commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "method not supported", http.StatusNotFound)
+		return
+	}
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/form" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Hello %s, your address is %s", name, address)
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+
+	fmt.Printf("Starting Server on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
